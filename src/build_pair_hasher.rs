5886688f1bc317b14c9fc5bcd773158bb61bd64d
VLAD EMINOVICI <veminovici@hotmail.com>
2024-02-29 08:38:02 +0200

    Refactor the code

--- src/build_pair_hasher.rs
+++ src/build_pair_hasher.rs
@@ -1,79 +1,11 @@
-use crate::{
-    build_siphasher::{BuildSipHasher, SipHasherKeys},
-    hash_iter::MultiHashIterator,
-    HasherExt,
-};
+use std::hash::BuildHasher;
+
 use rand::rngs::ThreadRng;
-use std::hash::{BuildHasher, Hasher};
 
-//
-// PairHasher - a combinator of two hashers which
-// can generate a sequence of hash values.
-//
-
-/// A [`Hasher`] which combines two [`Hasher`] instances. The hasher combinator
-/// uses the two [`Hasher`] instances to generate sequences of hash values.
-///
-/// # Example
-///
-///```
-/// use aabel_multihash_rs::*;
-/// use std::hash::{BuildHasher, Hash};
-///
-/// let keys1 = (0, 0);
-/// let keys2 = (1, 1);
-/// let mut hasher = BuildPairHasher::new_with_keys(keys1, keys2).build_hasher();
-///
-/// let item = "Hello world";
-/// item.hash(&mut hasher);
-///
-/// const HASHES_COUNT: usize = 10;
-/// let hashes = hasher.finish_iter().take(HASHES_COUNT).collect::<Vec<_>>();
-/// assert!(hashes.iter().all(|h| *h != 0));
-///```
-pub struct PairHasher<H1, H2> {
-    hasher1: H1,
-    hasher2: H2,
-}
-
-impl<H1, H2> PairHasher<H1, H2> {
-    pub(crate) fn new(hasher1: H1, hasher2: H2) -> Self {
-        Self { hasher1, hasher2 }
-    }
-}
-
-impl<H1, H2> Hasher for PairHasher<H1, H2>
-where
-    H1: Hasher,
-    H2: Hasher,
-{
-    fn finish(&self) -> u64 {
-        let a = self.hasher1.finish();
-        let b = self.hasher2.finish();
-        a.wrapping_add(b)
-    }
-
-    fn write(&mut self, bytes: &[u8]) {
-        self.hasher1.write(bytes);
-        self.hasher2.write(bytes);
-    }
-}
-
-impl<H1, H2> HasherExt for PairHasher<H1, H2>
-where
-    H1: Hasher,
-    H2: Hasher,
-{
-    fn finish_iter(self) -> impl Iterator<Item = u64> {
-        let a = self.hasher1.finish();
-        let b = self.hasher2.finish();
-        MultiHashIterator::new(a, b)
-    }
-}
-
-//
-// A builder for PairHasher.
-//
+use crate::{
+    build_sip_hasher::{BuildSipHasher, SipHasherKeys},
+    pair_hasher::PairHasher,
+};
 
 /// An instance of [`BuildHasher`] trait which builds [PairHasher] instances.
 ///
@@ -138,7 +70,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{BuildHasherExt, BuildPairHasher};
+    use crate::{BuildHasherExt, Hash64, HasherExt};
     use std::hash::{BuildHasher, Hash};
 
     #[test]
@@ -152,7 +84,7 @@ mod tests {
         const HASHES_COUNT: usize = 10;
 
         let hashes = hasher.finish_iter().take(HASHES_COUNT).collect::<Vec<_>>();
-        assert!(hashes.iter().all(|h| *h != 0));
+        assert!(hashes.iter().all(|h| h != &Hash64::from(0)));
     }
 
     #[test]
---
+++ tests/integration_tests.rs
@@ -0,0 +1,22 @@
+use std::hash::BuildHasher;
+
+use aabel_multihash_rs::{self, BuildHasherExt, BuildPairHasher, Hash64};
+
+#[test]
+fn it_test() {
+    let keys1 = (0, 0);
+    let keys2 = (1, 1);
+    let builder = BuildPairHasher::new_with_keys(keys1, keys2);
+
+    let item = "Hello world!";
+    const HASHES_COUNT: usize = 10;
+
+    let hash = builder.hash_one(item);
+    assert_ne!(hash, 0);
+
+    let hashes = builder
+        .hashes_one(item)
+        .take(HASHES_COUNT)
+        .collect::<Vec<_>>();
+    assert!(hashes.into_iter().all(|hash| hash != Hash64::from(0)));
+}
