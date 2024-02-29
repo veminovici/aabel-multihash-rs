75898e21bc7714f65c69d2d0ccb939ef986108de
VLAD EMINOVICI <veminovici@hotmail.com>
2024-02-29 09:04:01 +0200

    Update the integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -3,7 +3,7 @@ use std::hash::BuildHasher;
 use aabel_multihash_rs::{self, BuildHasherExt, BuildPairHasher, Hash64};
 
 #[test]
-fn it_test() {
+fn get_hashes() {
     let keys1 = (0, 0);
     let keys2 = (1, 1);
     let builder = BuildPairHasher::new_with_keys(keys1, keys2);
